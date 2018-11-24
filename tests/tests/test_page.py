dd9bc5f4c0c08aa563ce6596ccc63acb2b021d2c
Matthew Schinckel <matt@schinckel.net>
2018-11-24 12:23:31 +1030

    Add missing tests file.

---
+++ tests/tests/test_page.py
@@ -0,0 +1,56 @@
+import pytest
+
+from keyset_pagination.paginator import KeysetPaginator, InvalidPage
+
+from ..models import Event
+
+
+@pytest.fixture
+def events():
+    Event.objects.bulk_create([
+        Event(timestamp='2017-01-01T01:23:45Z', group="bar", reading=2),
+        Event(timestamp='2017-01-01T01:23:45Z', group="baz", reading=3),
+        Event(timestamp='2017-01-01T01:23:45Z', group="foo", reading=1),
+        Event(timestamp='2017-01-01T01:23:45Z', group="qux", reading=4),
+        Event(timestamp='2017-01-01T05:23:45Z', group="foo", reading=5),
+        Event(timestamp='2017-01-01T06:23:45Z', group="foo", reading=6),
+    ])
+
+
+def test_has_next_previous(events):
+    paginator = KeysetPaginator(Event.objects.order_by('-timestamp', 'group'), 5)
+    page = paginator.page(None)
+
+    assert page.has_next()
+    assert not page.has_previous()
+    assert page.start_index() == 1
+    assert page.end_index() == 5
+    assert page.page_index == 1
+
+    page = paginator.page(page.next_page_number())
+    assert not page.has_next()
+    assert page.has_previous()
+    assert page.start_index() == 6
+    assert page.page_index == 2
+
+    page = paginator.page(page.previous_page_number())
+    assert page.has_next()
+    assert not page.has_previous()
+    assert page.page_index == 1
+
+
+def test_empty_results():
+    paginator = KeysetPaginator(Event.objects.order_by('-timestamp', 'group'), 5)
+    page = paginator.page(None)
+
+    assert not page.has_next()
+    assert not page.has_previous()
+    assert page.page_index == 1
+    assert page.start_index() == 0
+    assert page.end_index() == 0
+
+
+def test_invalid_page():
+    paginator = KeysetPaginator(Event.objects.order_by('-timestamp', 'group'), 5)
+    with pytest.raises(InvalidPage):
+        paginator.page('["foo","bar"]')
